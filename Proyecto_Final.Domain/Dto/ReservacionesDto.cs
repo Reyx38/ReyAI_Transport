74831b5583cb1f63d43a618853271db8c8c1cf46
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-11-26 00:32:36 -0400

    DTO de Reservaciones

---
+++ Proyecto_Final.Domain/Dto/ReservacionesDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Final.Domain.Dto;
+
+public class ReservacionesDto
+{
+    public int ReservacionId { get; set; }
+
+    public int ViajeId { get; set; }
+    
+    public bool Pago { get; set; }
+
+    public string? Recibo { get; set; }
+}
