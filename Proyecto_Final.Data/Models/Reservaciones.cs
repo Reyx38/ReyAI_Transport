1ba033aafaa2071a76098c2678ede562e9aaf3dc
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-11-26 00:31:32 -0400

    Creacion del modelo Reservaciones

---
+++ Proyecto_Final.Data/Models/Reservaciones.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Final.Data.Models;
+
+public class Reservaciones
+{
+    public int ReservacionId { get; set; }
+
+    [ForeignKey("Viaje")]
+    public int ViajeId { get; set; }
+    public Viajes? Viaje { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    public bool Pago { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    public string? Recibo { get; set; }
+
+}
